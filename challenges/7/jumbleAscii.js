293019c5962f158f3ad805b4b5da09046fe8a68f
Aviya Developer <aviya.developer@gmail.com>
2019-12-06 13:44:07 +0200

    Adjusted level 7

--- challenges/7/jumbleAscii.js
+++ challenges/7/jumbleAscii.js
@@ -5,19 +5,19 @@ const jumbleAscii = ascii => {
     if (i % 2 === 0) {
       line = asciiLineArray[i]
         .split('')
+        .reverse()
         .join('</span><span class="character">');
       line = `<span class="character">${line}</span>`;
       asciiLineArray[i] = `<span class="line">${line}</span>`;
     } else {
       line = asciiLineArray[i]
         .split('')
-        .reverse()
         .join('</span><span class="character">');
       line = `<span class="character">${line}</span>`;
       asciiLineArray[i] = `<span class="line">${line}</span>`;
     }
   }
-  jumbledAscii = asciiLineArray.reverse().join('\n');
+  jumbledAscii = asciiLineArray.join('\n');
   jumbledAscii = `<span class="all">${jumbledAscii}</span>`;
   return jumbledAscii;
 };
